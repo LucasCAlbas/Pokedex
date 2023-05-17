a1ae9304ebef495d04047e66b56361860b4ee4a8
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2023-05-17 11:13:46 -0300

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,12 +5,16 @@ import React, { useEffect, useState } from 'react';
 
 // Primeiro, vamos criar o componente PokemonCard
 // Este componente recebe um pokemon como prop e exibe o nome e a imagem do pokemon
-export default function PokemonCard({ pokemon }) {
+export default function PokemonCard({pokemon}) {
   
   return (
     <div>
       <h2>{pokemon.name}</h2>
-      {/* <img src={pokemon.sprites.front_default} alt={pokemon.name} /> */}
+       <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+        <div>
+          {pokemon.types.map((item) =>(<h3>{item.type.name}</h3>))}
+          {pokemon.types[0].type.name}
+       </div>
     </div>
   );
 }
